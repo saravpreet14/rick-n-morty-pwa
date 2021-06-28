d708674f8ea6f59bb54fafe1c4c180a78a5ecc89
Phanindra180 <phanindrasai180@gmail.com>
2021-06-28 12:22:11 +0530

    changed img in characterCard -2, by Phanindra

--- components/characterCard.js
+++ components/characterCard.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import styles from "./character.module.css";
+import Image from "next/image";
 
 export default function Card(props) {
   return (
     <div className={styles.characterCard}>
       <Link href={props.url}>
         <a>
-          <img src={props.image} alt={props.name} />
+          <Image src={props.image} alt={props.name} width="300" height="300" />
           <h3>{props.name}</h3>
         </a>
       </Link>
