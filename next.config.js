5fdfaab00db0231c818e729b5ff533352161d316
Phanindra180 <phanindrasai180@gmail.com>
2021-07-01 12:42:11 +0530

    installable

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   // presets: [require.resolve("next/babel")],
   presets: ["@babel/preset-env", "@babel/preset-react"],
   images: {
     domains: ["rickandmortyapi.com"],
   },
-};
+});
--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import {
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import { Provider } from "next-auth/client";
+import Head from "next/head";
 // const client = new ApolloClient({
 //   uri: "https://48p1r2roz4.sse.codesandbox.io",
 //   cache: new InMemoryCache(),
@@ -23,6 +24,33 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
       <ApolloProvider client={client}>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>Next.js PWA Example</title>
+
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/icons/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/icons/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#317EFB" />
+        </Head>
         <Component {...pageProps} />
       </ApolloProvider>
     </Provider>
