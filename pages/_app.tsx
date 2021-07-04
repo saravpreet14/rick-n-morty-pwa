40d5d9e20324ec971892d4d35cedcda18ddafaf4
Phanindra180 <phanindrasai180@gmail.com>
2021-07-04 17:11:21 +0530

    changed app.tsx, by Phanindra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import {
   gql,
 } from "@apollo/client";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { onError } from "@apollo/client/link/error";
 import { Provider } from "next-auth/client";
 // const client = new ApolloClient({
@@ -23,7 +24,33 @@ const client = new ApolloClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
-      <ApolloProvider client={client}>
+      <ApolloProvider client={client}>        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>Next.js PWA Example</title>
+
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/icons/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/icons/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#317EFB" />
+        </Head>
         <Component {...pageProps} />
       </ApolloProvider>
     </Provider>
